48a3448fce0e4475c04158dc06632365eb9047cf
ohitsel <81478153+SelVer99@users.noreply.github.com>
2021-06-04 18:15:26 +0200

    bozza di commento
    per il momento commentare aggiorna una label

--- BeeWork/src/app/pages/dettagli-task/dettagli-task.page.ts
+++ BeeWork/src/app/pages/dettagli-task/dettagli-task.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dettagli-task.page.scss'],
 })
 export class DettagliTaskPage implements OnInit {
+  myVal = '';
+  myVal1 = '';
 
   constructor() { }
 
+
   ngOnInit() {
   }
+
+  submit(){
+    this.myVal1 = this.myVal;
+    this.myVal='';
+  }
+
 
 }
