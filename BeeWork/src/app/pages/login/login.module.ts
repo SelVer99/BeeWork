30af263970680d23c8dc6bb581045e59036c6f30
AngeloC99 <angelocasciani@live.it>
2021-06-09 20:01:52 +0200

    Fatti html per login e registrazione

--- BeeWork/src/app/pages/login/login.module.ts
+++ BeeWork/src/app/pages/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,13 +10,20 @@ import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 import {TranslateModule} from '@ngx-translate/core';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginPage
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     TranslateModule.forChild(),
-    LoginPageRoutingModule
+    RouterModule.forChild(routes)
   ],
   declarations: [LoginPage]
 })
